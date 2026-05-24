d7ad15a7e71d03d1a532c166c236a51d3fb30d0b
Deivy Santiago Santiago Mendoza <deivysantiago.dev@gmail.com>
2026-05-23 19:21:34 -0500

    card product chef

--- src/features/orderchef/components/PageOrderChef.tsx
+++ src/features/orderchef/components/PageOrderChef.tsx
@@ -1,10 +1,14 @@
 import HeaderChef from "@/shared/components/ui/HeaderChef.tsx";
+import GridOrderChef from "./GridOrderChef.tsx";
 
 const PageOrderChef = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 bg-background h-screen">
       <div>
         <HeaderChef />
+      </div>
+      <div className="pt-7">
+        <GridOrderChef />
       </div>
     </div>
   );
